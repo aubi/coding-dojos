89256d24ffc6e6907378a8a42f8ede8ead7dfa0c
James Hillyard <james.hillyard@payara.fish>
2023-04-20 09:25:19 +0100

    Setup JUnit
    Signed-off-by: James Hillyard <james.hillyard@payara.fish>

---
+++ src/test/java/fish/payara/codingdojo/eightqueens/EightQueensTest.java
@@ -0,0 +1,14 @@
+package fish.payara.codingdojo.eightqueens;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class EightQueensTest {
+
+    @Test
+    public void testThisWorks() {
+        assertTrue(true);
+    }
+
+}

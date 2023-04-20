efe14a3a2eddd3d691f113eb365a365c6b61c8de
Petr Aubrecht <aubrecht@asoftware.cz>
2023-04-20 11:00:17 +0200

    attempt to solution

--- src/main/java/fish/payara/codingdojo/eightqueens/Board.java
+++ src/main/java/fish/payara/codingdojo/eightqueens/Board.java
@@ -1,7 +1,7 @@
 package fish.payara.codingdojo.eightqueens;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 /**
  *
@@ -11,12 +11,12 @@ public class Board {
 
     private int size;
     private int numberOfPieces;
-    private String[] positions;
+    private String[][] positions;
 
     public Board(int size) {
         this.size = size;
         this.numberOfPieces = size;
-        this.positions = new String[getSize()];
+        this.positions = new String[size][size];
     }
 
     int getSize() {
@@ -28,10 +28,18 @@ public class Board {
     }
 
     public List<String> getAllSolutions() {
-        Random random = new Random();
-        for(int i = 0; i < getSize(); i++){
-            positions[random.nextInt(getSize())] = "Q";
+        //Random random = new Random();
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                positions[x][y] = ".";
+            }
         }
+        List<String> solutions = new ArrayList<>();
+        solve(0, 0, solutions);
+
+//        for (int i = 0; i < getSize(); i++) {
+//            positions[random.nextInt(getSize())] = "Q";
+//        }
         return null;
     }
 
@@ -41,5 +49,24 @@ public class Board {
             return "[Q]";
         }
         return "";
+    }
+
+    /**
+     * Try new queen on [x,y]
+     */
+    private void solve(int x, int y, List<String> solutions) {
+        positions[x][y] = "Q";
+        // TODO: check if this is solution
+        // at the end
+        // not conflicting
+        // add to solution
+
+        // TODO: try next line
+        solve(0, y + 1, solutions);
+
+        // cleanup
+        positions[x][y] = ".";
+
+        //? solve(x+1, y, solution)
     }
 }

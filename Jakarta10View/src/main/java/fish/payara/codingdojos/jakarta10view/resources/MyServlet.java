e03c6f2af871f03c5ee543b195fac5c041113d3b
Kalin Chan <kalin.chan@payara.fish>
2023-05-09 10:07:23 +0100

    add servlet and jsp files

--- Jakarta10View/src/main/java/fish/payara/codingdojos/jakarta10view/resources/MyServlet.java
+++ Jakarta10View/src/main/java/fish/payara/codingdojos/jakarta10view/resources/MyServlet.java
@@ -1,6 +1,19 @@
 package fish.payara.codingdojos.jakarta10view.resources;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+@WebServlet(name = "MyServlet", urlPatterns = "/test.exe")
 public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("Test");
+    }
 }

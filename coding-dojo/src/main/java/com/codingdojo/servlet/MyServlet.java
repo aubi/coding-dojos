1ff98f93b69c257176819bb463b2dd3f6e6b703e
Arie Kiswanto <arie.kiswanto@payara.fish>
2023-07-18 18:48:39 +0800

    Added servlet

---
+++ coding-dojo/src/main/java/com/codingdojo/servlet/MyServlet.java
@@ -0,0 +1,18 @@
+package com.codingdojo.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "MyServlet", urlPatterns = {"/hello-servlet"})
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Hello! Coding Dojo");
+    }
+}

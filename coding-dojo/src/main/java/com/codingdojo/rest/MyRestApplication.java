50f8199aa820e8c019456dd65d9bce03897b9164
Shubham Agrawal <shub8968@gmail.com>
2023-07-18 16:00:44 +0530

    Add files

---
+++ coding-dojo/src/main/java/com/codingdojo/rest/MyRestApplication.java
@@ -0,0 +1,9 @@
+package com.codingdojo.rest;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("")
+public class MyRestApplication extends Application {
+}

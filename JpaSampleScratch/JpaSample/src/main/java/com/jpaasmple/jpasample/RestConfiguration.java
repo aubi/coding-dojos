5fed6aab43a9705e55dfdcad555491dd561925bb
Marcelo Gonçalves <marcelocg@gmail.com>
2023-11-21 10:24:20 +0000

    Add configuration

---
+++ JpaSampleScratch/JpaSample/src/main/java/com/jpaasmple/jpasample/RestConfiguration.java
@@ -0,0 +1,9 @@
+package com.jpaasmple.jpasample;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class RestConfiguration extends Application {
+
+}

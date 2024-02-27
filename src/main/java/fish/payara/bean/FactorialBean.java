9ffc2f568e7337f9950ed883d87ae86646e4ac03
Alfonso Altamirano <alfonso.altamirano@payara.fish>
2024-02-27 08:36:21 -0600

    starting bean

---
+++ src/main/java/fish/payara/bean/FactorialBean.java
@@ -0,0 +1,10 @@
+package fish.payara.bean;
+
+public class FactorialBean {
+    
+    
+    public void makeFactorial(String input) {
+        String[] inputs = input.split(",");
+        
+    }
+}

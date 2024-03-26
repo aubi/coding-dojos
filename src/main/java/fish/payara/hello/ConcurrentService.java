b8acd1897283a55e9fa8bd16f3d2f65699dbc34b
Kalin Chan <kalin.chan@payara.fish>
2024-03-26 14:55:12 +0000

    add jsf

--- src/main/java/fish/payara/hello/ConcurrentService.java
+++ src/main/java/fish/payara/hello/ConcurrentService.java
@@ -4,11 +4,13 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.Resource;
 import jakarta.ejb.Stateless;
 import jakarta.enterprise.concurrent.ManagedScheduledExecutorService;
+import jakarta.inject.Named;
 
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Stateless
+@Named
 public class ConcurrentService {
     @Resource
     ManagedScheduledExecutorService mes;
@@ -19,6 +21,7 @@ public class ConcurrentService {
             System.out.println("##################### Running currently. Count --> " + count.incrementAndGet());
         }, 0, 1, TimeUnit.SECONDS);
     }
+    
 
     public AtomicInteger printValue() {
         return count;

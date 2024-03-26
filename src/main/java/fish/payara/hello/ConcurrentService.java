e7fae5794208e275d7c8917fa018eaebe27436ed
Luqman Saeed <luqman.saeed@payara.fish>
2024-03-26 15:05:21 +0000

    Stop method

--- src/main/java/fish/payara/hello/ConcurrentService.java
+++ src/main/java/fish/payara/hello/ConcurrentService.java
@@ -6,6 +6,7 @@ import jakarta.ejb.Stateless;
 import jakarta.enterprise.concurrent.ManagedScheduledExecutorService;
 import jakarta.inject.Named;
 
+import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -14,16 +15,24 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ConcurrentService {
     @Resource
     ManagedScheduledExecutorService mes;
+    ScheduledFuture<?> scheduledFuture;
     final AtomicInteger count = new AtomicInteger();
     @PostConstruct
     void init() {
-        mes.scheduleAtFixedRate(() -> {
+        scheduledFuture = mes.scheduleAtFixedRate(() -> {
             System.out.println("##################### Running currently. Count --> " + count.incrementAndGet());
         }, 0, 1, TimeUnit.SECONDS);
+
     }
     
 
     public AtomicInteger printValue() {
         return count;
+    }
+
+    public void stop() {
+        if (scheduledFuture != null) {
+            scheduledFuture.cancel(true);
+        }
     }
 }

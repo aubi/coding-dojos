08e8cb32deb2bc49612835628556ec5a1835a4e1
Andrew Pielage <pandrex247@hotmail.com>
2024-03-26 15:13:38 +0000

    Add stop and reset
    Signed-off-by: Andrew Pielage <pandrex247@hotmail.com>

--- src/main/java/fish/payara/hello/ConcurrentService.java
+++ src/main/java/fish/payara/hello/ConcurrentService.java
@@ -20,11 +20,10 @@ public class ConcurrentService {
     @PostConstruct
     void init() {
         scheduledFuture = mes.scheduleAtFixedRate(() -> {
-            System.out.println("##################### Running currently. Count --> " + count.incrementAndGet());
+            System.out.println("##################### Running concurrently. Count --> " + count.incrementAndGet());
         }, 0, 1, TimeUnit.SECONDS);
 
     }
-    
 
     public AtomicInteger printValue() {
         return count;
@@ -34,5 +33,15 @@ public class ConcurrentService {
         if (scheduledFuture != null) {
             scheduledFuture.cancel(true);
         }
+    }
+
+    public void start() {
+        scheduledFuture = mes.scheduleAtFixedRate(() -> {
+            System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Running concurrently after restarting. Count --> " + count.incrementAndGet());
+        }, 0, 1, TimeUnit.SECONDS);
+    }
+
+    public void reset() {
+        count.set(0);
     }
 }

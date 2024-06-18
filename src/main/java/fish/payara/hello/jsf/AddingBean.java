0681032144601e224c1365c5d35dd94915e715eb
NotedSalmon <moreiraandre2001@gmail.com>
2024-06-18 16:05:43 +0100

    Broken :(

--- src/main/java/fish/payara/hello/jsf/AddingBean.java
+++ src/main/java/fish/payara/hello/jsf/AddingBean.java
@@ -4,13 +4,16 @@
  */
 package fish.payara.hello.jsf;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.enterprise.context.SessionScoped;
+import jakarta.faces.model.ListDataModel;
 import jakarta.inject.Named;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  *
@@ -22,9 +25,18 @@ public class AddingBean implements Serializable {
 
     private double result;
     private List<Double> history = new ArrayList<>();
+    private ListDataModel<Double> historyDataModel;
     
     private Double valueA;
-    
+
+    @PostConstruct
+    public void init(){
+        historyDataModel = new ListDataModel<>(history);
+    }
+
+    public void removeNum(){
+        history.remove(historyDataModel.getRowData());
+    }
 
     public void calc() {
         history.add(valueA);

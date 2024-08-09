5ae494db4755531846e9d591dcadcb737be21f66
Petr Aubrecht <aubrecht@asoftware.cz>
2024-08-09 13:05:11 +0200

    calculate the times only once, use getter only to return results

--- WatchDog/src/main/java/fish/payara/watchdog/jsf/Servers.java
+++ WatchDog/src/main/java/fish/payara/watchdog/jsf/Servers.java
@@ -1,5 +1,6 @@
 package fish.payara.watchdog.jsf;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 
@@ -7,7 +8,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.sql.Time;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +20,8 @@ import java.util.List;
 @RequestScoped
 public class Servers {
     private List<String> servers = new ArrayList<>();
+    private List<TimeMeasurement> times = new ArrayList<>();
+
     /**
      * Creates a new instance of Servers
      */
@@ -27,6 +29,29 @@ public class Servers {
         servers.add("http://google.com");
         servers.add("http://seznam.cz");
         servers.add("not-an-uri");
+    }
+
+    @PostConstruct
+    public void calculateTimes() {
+        for (String s : servers) {
+            URI uri = URI.create(s);
+            try {
+                long start = System.currentTimeMillis();
+
+                HttpClient client = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(uri)
+                        .build();
+                client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                        .thenApply(HttpResponse::body)
+                        .thenAccept(System.out::println)
+                        .join();
+                long end = System.currentTimeMillis();
+                times.add(new TimeMeasurement(uri, Duration.ofMillis(end - start), null));
+            } catch (Exception e) {
+                times.add(new TimeMeasurement(uri, null, e.getMessage()));
+            }
+        }
     }
 
     public List<String> getList() {
@@ -38,26 +63,6 @@ public class Servers {
     }
 
     public List<TimeMeasurement> getTimes() {
-        List<TimeMeasurement> times = new ArrayList<>();
-        for (String s : servers) {
-            URI uri = URI.create(s);
-            try {
-                long start = System.currentTimeMillis();
-
-                HttpClient client = HttpClient.newHttpClient();
-                HttpRequest request = HttpRequest.newBuilder()
-                                                 .uri(uri)
-                                                 .build();
-                client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                      .thenApply(HttpResponse::body)
-                      .thenAccept(System.out::println)
-                      .join();
-                long end = System.currentTimeMillis();
-                times.add(new TimeMeasurement(uri, Duration.ofMillis(end - start), null));
-            } catch (Exception e) {
-                times.add(new TimeMeasurement(uri, null, e.getMessage()));
-            }
-        }
         return times;
     }
 

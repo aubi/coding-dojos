131014c7148e741bb660f64e176b6f50a67d00fc
Rohan Parekh <rohan.parekh.in@gmail.com>
2024-09-03 18:24:12 +0530

    Refactor repeat "I"

--- RomanNumerals/src/main/java/fish/payara/romannumerals/RomanNumerals.java
+++ RomanNumerals/src/main/java/fish/payara/romannumerals/RomanNumerals.java
@@ -15,14 +15,20 @@ public class RomanNumerals {
         if (num == 4) {
             return "IV";
         } else if (num >= 5 && num < 9) {
-            return "V" + "I".repeat(num-5);
+            return "V" + repeatI(num, 5);
         } else if (num == 9) {
             return "IX";
         } else if (num >= 10 && num < 14) {
-            return "X" + "I".repeat(num-10);
+            return "X" + repeatI(num, 10);
         } else if (num == 14) {
             return "XIV";
+        } else if (num >= 15 && num < 19) {
+            return "XV" + repeatI(num, 15);
         }
         return "I".repeat(num);
+    }
+
+    private static String repeatI(int num, int count) {
+        return "I".repeat(num - count);
     }
 }

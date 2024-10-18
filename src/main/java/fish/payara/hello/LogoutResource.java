6e7a1fb336397d41705006acdbd4c66088c1c000
Raushan <raushankumar606@gmail.com>
2024-10-18 16:11:12 +0530

    logout endpoint

---
+++ src/main/java/fish/payara/hello/LogoutResource.java
@@ -0,0 +1,25 @@
+package fish.payara.hello;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("logout")
+public class LogoutResource {
+
+    @Inject
+    private HttpServletRequest request;
+
+    @GET
+    public String logout() throws ServletException {
+        request.logout();
+        var session = request.getSession(false);
+        if (session != null) {
+            session.invalidate();
+        }
+        return "You have been logged out";
+    }
+
+}

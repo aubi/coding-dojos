97793cf0b6daf32e54fee3c04d308c686e4a492e
Petr Aubrecht <aubrecht@asoftware.cz>
2024-10-18 14:17:29 +0200

    specify default values, just to be sure

--- src/main/java/fish/payara/hello/HelloWorldResource.java
+++ src/main/java/fish/payara/hello/HelloWorldResource.java
@@ -1,29 +1,41 @@
 package fish.payara.hello;
 
-import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import jakarta.ws.rs.core.SecurityContext;
+import java.security.Principal;
 
 @Path("hello")
 public class HelloWorldResource {
 
-    @Inject
-    @ConfigProperty(name = "defaultName", defaultValue = "world")
-    private String defaultName;
+//    @Inject
+//    @ConfigProperty(name = "defaultName", defaultValue = "world")
+//    private String defaultName;
+
+    @Context
+    private SecurityContext security;
 
     @GET
     public Response hello(@QueryParam("name") String name) {
         if ((name == null) || name.trim().isEmpty()) {
-            name = defaultName;
+            name = "defaultName";
         }
         return Response
                 .ok(name)
                 .build();
     }
 
+    @GET
+    @Path("me")
+    public Response me() {
+        Principal userPrincipal = security.getUserPrincipal();
+        return Response
+                .ok(userPrincipal == null ? "Not logged in" : userPrincipal.getName())
+                .build();
+    }
     
 
 }

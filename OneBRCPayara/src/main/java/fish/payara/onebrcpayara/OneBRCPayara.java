18c9c247170f81ad154e5b46104dfadba2aa3270
Petr Aubrecht <aubrecht@asoftware.cz>
2024-11-29 18:16:51 +0100

    write result to a file

--- OneBRCPayara/src/main/java/fish/payara/onebrcpayara/OneBRCPayara.java
+++ OneBRCPayara/src/main/java/fish/payara/onebrcpayara/OneBRCPayara.java
@@ -1,6 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 package fish.payara.onebrcpayara;
 
 import java.io.BufferedReader;
@@ -8,7 +5,11 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -18,7 +19,7 @@ import java.util.concurrent.Executors;
  */
 public class OneBRCPayara {
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
+    public static void main(String[] args) throws FileNotFoundException, IOException, InterruptedException {
         System.out.println("Let's read the file");
         System.out.println("Available CPU Cores:" + Runtime.getRuntime().availableProcessors());
 
@@ -35,7 +36,7 @@ public class OneBRCPayara {
                 if (!stats.containsKey(lineParts[0])) {
                     stats.put(lineParts[0], new ArrayList<>());
                 }
-                stats.get(lineParts[0]).add(Double.parseDouble(lineParts[1]));
+                stats.get(lineParts[0]).add(Double.valueOf(lineParts[1]));
             }
             try (ExecutorService es = Executors.newVirtualThreadPerTaskExecutor()) {
                 for (Map.Entry<String, List<Double>> entry : stats.entrySet()) {
@@ -52,7 +53,7 @@ public class OneBRCPayara {
                             }
                             sum += f;
                         }
-                        System.out.println(new Statistics(entry.getKey(), min, (sum / entry.getValue().size()), max));
+                        out.println(new Statistics(entry.getKey(), min, (sum / entry.getValue().size()), max));
                     });
                 }
             }

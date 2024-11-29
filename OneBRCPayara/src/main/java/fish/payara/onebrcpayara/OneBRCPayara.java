9e383d2775e45c35d18348e47407de1b941b2173
Fabio Turizo <fabio.turizo@gmail.com>
2024-11-29 10:56:27 -0500

    Added virtual threads to parallelize

--- OneBRCPayara/src/main/java/fish/payara/onebrcpayara/OneBRCPayara.java
+++ OneBRCPayara/src/main/java/fish/payara/onebrcpayara/OneBRCPayara.java
@@ -9,6 +9,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  *
@@ -18,6 +20,7 @@ public class OneBRCPayara {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
         System.out.println("Let's read the file");
+        System.out.println("Available CPU Cores:" + Runtime.getRuntime().availableProcessors());
 
         Map<String, List<Double>> stats = new TreeMap<>();
         try (Reader file = new BufferedReader(new FileReader("OneBRCPayara/data/weather_stations.csv"), 1024 * 1024)) {
@@ -29,25 +32,29 @@ public class OneBRCPayara {
                 String line = scanner.nextLine();
                 String[] lineParts = line.split(";");
                 //System.out.println(Arrays.toString(lineParts));
-                if(!stats.containsKey(lineParts[0])) {
+                if (!stats.containsKey(lineParts[0])) {
                     stats.put(lineParts[0], new ArrayList<>());
                 }
                 stats.get(lineParts[0]).add(Double.parseDouble(lineParts[1]));
             }
-            for (Map.Entry<String, List<Double>> entry : stats.entrySet()) {
-                double min = Double.POSITIVE_INFINITY;
-                double max = Double.NEGATIVE_INFINITY;
-                double sum = 0f;
-                for(double f : entry.getValue()) {
-                    if(f < min) {
-                        min = f;
-                    }
-                    if(f > max) {
-                        max = f;
-                    }
-                    sum += f;
+            try (ExecutorService es = Executors.newVirtualThreadPerTaskExecutor()) {
+                for (Map.Entry<String, List<Double>> entry : stats.entrySet()) {
+                    es.submit(() -> {
+                        double min = Double.POSITIVE_INFINITY;
+                        double max = Double.NEGATIVE_INFINITY;
+                        double sum = 0f;
+                        for (double f : entry.getValue()) {
+                            if (f < min) {
+                                min = f;
+                            }
+                            if (f > max) {
+                                max = f;
+                            }
+                            sum += f;
+                        }
+                        System.out.println(new Statistics(entry.getKey(), min, (sum / entry.getValue().size()), max));
+                    });
                 }
-                System.out.println(new Statistics(entry.getKey(), min, (sum/entry.getValue().size()), max));
             }
         }
     }

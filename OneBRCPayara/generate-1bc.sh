a6508e24d4441f7537203b0b8f19603176bd5b66
Petr Aubrecht <aubrecht@asoftware.cz>
2025-01-22 10:50:47 +0100

    add netbeans setup with command line params, increase amount of data in the script

--- OneBRCPayara/generate-1bc.sh
+++ OneBRCPayara/generate-1bc.sh
@@ -2,9 +2,9 @@
 tail --lines=+3 data/weather_stations.csv > ../weather_stations.csv-headless
 cp data/weather_stations.csv ../weather_stations.csv
 # should run to 2300
-for i in {1..2500}
+for i in {1..25000}
 do
-    echo "${i}/2300"
+    echo "${i}/25000"
     cat ../weather_stations.csv-headless >> ../weather_stations.csv
     # wc -l ../weather_stations.csv
 done

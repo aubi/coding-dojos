cafe7d3954162fb32440902a77f79f6eb95b8a6e
Ismah Hussain <ismahhussain@hotmail.co.uk>
2025-04-07 13:48:32 +0100

    refactoring

--- tdd/src/main/java/org/example/Calculator.java
+++ tdd/src/main/java/org/example/Calculator.java
@@ -2,18 +2,18 @@ package org.example;
 
 public class Calculator {
     public static int add (int value1, int value2) {
-        throw new UnsupportedOperationException();
+        return value1 + value2;
     }
 
     public static int subtract (int value1, int value2) {
-        throw new UnsupportedOperationException();
+        return value1 - value2;
     }
 
     public static double divide (int numerator, int denominator) {
-        throw new UnsupportedOperationException();
+        return (double) numerator / denominator;
     }
 
     public static int multiply (int value1, int value2) {
-        throw new UnsupportedOperationException();
+       return value1 * value2;
     }
 }
--- tdd/src/test/java/TestCalculator.java
+++ tdd/src/test/java/TestCalculator.java
@@ -8,27 +8,32 @@ public class TestCalculator {
     public void testAdd() {
         int number1 = 1;
         int number2 = 2;
-        Assert.assertEquals(Calculator.add(number1, number2), 3);
+        Assert.assertEquals(3, Calculator.add(number1, number2));
     }
 
     @Test
     public void testSubtract () {
         int n1 = 10;
         int n2 = 4;
-        Assert.assertEquals(Calculator.subtract(n1, n2), 6);
+        Assert.assertEquals(6, Calculator.subtract(n1, n2));
     }
 
     @Test
     public void testMultiply () {
         int n1 = 5;
         int n2 = 6;
-        Assert.assertEquals(Calculator.multiply(5, 6), 30);
+        Assert.assertEquals(30, Calculator.multiply(n1, n2));
     }
 
     @Test
     public void testDivide () {
         int n1 = 100;
         int n2 = 10;
-        Assert.assertEquals(Calculator.divide(n1, n2), 10.0);
+        Assert.assertEquals(10.0, Calculator.divide(n1, n2), 0.001);
+    }
+
+    @Test
+    public void multiplyAndDivide () {
+        int n1 = 10;
     }
 }

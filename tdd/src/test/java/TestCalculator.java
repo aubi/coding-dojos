99f466d07267a97872902279f0c3428ed6695a2c
NotedSalmon <moreiraandre2001@gmail.com>
2025-04-07 13:19:03 +0100

    start

---
+++ tdd/src/test/java/TestCalculator.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCalculator {
+
+    @Test
+    public void testAdd() {
+        int number1 = 1;
+        int number2 = 2;
+        Assert.assertEquals();
+    }
+}

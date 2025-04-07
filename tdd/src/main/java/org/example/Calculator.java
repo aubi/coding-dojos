2e008fb62eccb872ad0b26fc2383c93aa2b17448
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2025-04-07 13:28:25 +0100

    Add skeleton Calculator implementation and tests.

---
+++ tdd/src/main/java/org/example/Calculator.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Calculator {
+    public static int add (int value1, int value2) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int subtract (int value1, int value2) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static double divide (int numerator, int denominator) {
+        throw new UnsupportedOperationException();
+    }
+
+    public static int multiply (int value1, int value2) {
+        throw new UnsupportedOperationException();
+    }
+}
--- tdd/src/test/java/TestCalculator.java
+++ tdd/src/test/java/TestCalculator.java
@@ -1,3 +1,4 @@
+import org.example.Calculator;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,6 +8,27 @@ public class TestCalculator {
     public void testAdd() {
         int number1 = 1;
         int number2 = 2;
-        Assert.assertEquals();
+        Assert.assertEquals(Calculator.add(number1, number2), 3);
+    }
+
+    @Test
+    public void testSubtract () {
+        int n1 = 10;
+        int n2 = 4;
+        Assert.assertEquals(Calculator.subtract(n1, n2), 6);
+    }
+
+    @Test
+    public void testMultiply () {
+        int n1 = 5;
+        int n2 = 6;
+        Assert.assertEquals(Calculator.multiply(5, 6), 30);
+    }
+
+    @Test
+    public void testDivide () {
+        int n1 = 100;
+        int n2 = 10;
+        Assert.assertEquals(Calculator.divide(n1, n2), 10.0);
     }
 }

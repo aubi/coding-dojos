e8234ca1fd18c9411d54955110431f11a6af2ce9
adrian <astoica@gmail.com>
2025-04-16 17:57:26 +0300

    fixed import

--- src/main/java/fish/payara/test/rest/NumberApplication.java
+++ src/main/java/fish/payara/test/rest/NumberApplication.java
@@ -1,7 +1,9 @@
 package fish.payara.test.rest;
 
 import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.*;
 
 @ApplicationPath("/api")
-public class NumberApplication {
+public class NumberApplication extends Application {
+
 }

6c022bcd35bc2b6a2bd92840588adbe373dea15e
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2025-04-16 16:15:36 +0100

    Add sequence endpoint.

--- src/main/java/fish/payara/test/rest/resource/ApiResource.java
+++ src/main/java/fish/payara/test/rest/resource/ApiResource.java
@@ -4,6 +4,11 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
 
 @Path("/numbers")
 public class ApiResource {
@@ -11,7 +16,23 @@ public class ApiResource {
     @GET
     @Produces("text/plain")
     @Path("{number}")
-    public int getSquare(@PathParam("number") int number) {
+    public int getSquare(@PathParam("number") int number) throws InterruptedException {
+        Thread.sleep(1000);
         return number * number;
     }
+
+    @GET
+    @Produces("text/plain")
+    @Path("/sequence/{number}")
+    public int getSequenceSum (@PathParam("number") int number) {
+        Client client = ClientBuilder.newClient();
+        int sum = 0;
+        for (int i = 0; i < 5; ++i) {
+            sum += client.target("http://localhost:8080/dojo/api/numbers/" + (number + i))
+                .request(MediaType.TEXT_PLAIN)
+                .get(Integer.class);
+        }
+        return sum;
+    }
+
 }

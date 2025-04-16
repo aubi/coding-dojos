8fb66f147ab1d3269437310bb863b69bd6b7c1a3
adrian <astoica@gmail.com>
2025-04-16 18:39:31 +0300

    made reactive

--- src/main/java/fish/payara/test/rest/resource/ApiResource.java
+++ src/main/java/fish/payara/test/rest/resource/ApiResource.java
@@ -8,6 +8,9 @@ import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 @Path("/numbers")
@@ -24,18 +27,22 @@ public class ApiResource {
     @GET
     @Produces("text/plain")
     @Path("/sequence/{number}")
-    public int getSequenceSum (@PathParam("number") int number) throws InterruptedException, ExecutionException {
+    public int getSequenceSum(@PathParam("number") int number) throws InterruptedException, ExecutionException {
         Client client = ClientBuilder.newClient();
         int sum = 0;
+        List<CompletableFuture<Integer>> futures = new ArrayList<>();
         for (int i = 0; i < 5; ++i) {
-            sum += client.target("http://localhost:8080/coding-dojos/api/numbers/" + (number + i))
-                            .request(MediaType.TEXT_PLAIN)
-                            .rx()
-                            .get(Integer.class)
-                            .toCompletableFuture()
-                            .get();
+            futures.add(client.target("http://localhost:8080/coding-dojos/api/numbers/" + (number + i))
+                    .request(MediaType.TEXT_PLAIN)
+                    .rx()
+                    .get(Integer.class)
+                    .toCompletableFuture());
         }
+
+        for (CompletableFuture<Integer> future : futures) {
+            sum += future.get();
+        }
+
         return sum;
     }
-
 }

6fb706b2773a432a7c472e9a4bfe5482bcfbafea
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2025-04-28 14:14:44 +0100

    Replace double with BigDecimal.

--- tdd/src/main/java/fish/payara/calculator/Calculator.java
+++ tdd/src/main/java/fish/payara/calculator/Calculator.java
@@ -1,23 +1,26 @@
 package fish.payara.calculator;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
-    Simple calculator class.
-    <p>
-    This class provides basic arithmetic operations: addition, subtraction, multiplication, and division.
+ * Simple calculator class.
+ * <p> This class provides basic arithmetic operations: addition, subtraction, multiplication, and division. </p>
  */
 public class Calculator {
-    public static double add (double value1, double value2) {
-        return value1 + value2;
+    public static BigDecimal add (BigDecimal value1, BigDecimal value2) {
+        return value1.add(value2);
     }
 
-    public static double subtract (double value1, double value2) {
-        return value1 - value2;
+    public static BigDecimal subtract (BigDecimal value1, BigDecimal value2) {
+        return value1.subtract(value2);
     }
 
-    public static double divide (double numerator, double denominator) {
-        return numerator / denominator;
+    public static BigDecimal divide (BigDecimal numerator, BigDecimal denominator) {
+        return numerator.divide(denominator, RoundingMode.HALF_UP);
     }
 
-    public static double multiply (double value1, double value2) {
-       return value1 * value2;
+    public static BigDecimal multiply (BigDecimal value1, BigDecimal value2) {
+       return value1.multiply(value2);
     }
 }
--- tdd/src/test/java/fish/payara/calculator/TestCalculator.java
+++ tdd/src/test/java/fish/payara/calculator/TestCalculator.java
@@ -3,40 +3,42 @@ package fish.payara.calculator;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 public class TestCalculator {
 
     @Test
     public void testAdd() {
-        int number1 = 1;
-        int number2 = 2;
-        Assert.assertEquals(3, Calculator.add(number1, number2), 0);
+        BigDecimal number1 =BigDecimal.valueOf(1);
+        BigDecimal number2 = BigDecimal.valueOf(2);
+        Assert.assertEquals(BigDecimal.valueOf(3), Calculator.add(number1, number2));
     }
 
     @Test
     public void testSubtract () {
-        int n1 = 10;
-        int n2 = 4;
-        Assert.assertEquals(6, Calculator.subtract(n1, n2), 0);
+        BigDecimal n1 = BigDecimal.valueOf(10);
+        BigDecimal n2 = BigDecimal.valueOf(4);
+        Assert.assertEquals(BigDecimal.valueOf(6), Calculator.subtract(n1, n2));
     }
 
     @Test
     public void testMultiply () {
-        int n1 = 5;
-        int n2 = 6;
-        Assert.assertEquals(30, Calculator.multiply(n1, n2), 0);
+        BigDecimal n1 = BigDecimal.valueOf(5);
+        BigDecimal n2 = BigDecimal.valueOf(6);
+        Assert.assertEquals(BigDecimal.valueOf(30), Calculator.multiply(n1, n2));
     }
 
     @Test
     public void testDivide () {
-        int n1 = 100;
-        int n2 = 10;
-        Assert.assertEquals(10.0, Calculator.divide(n1, n2), 0.001);
+        BigDecimal n1 = BigDecimal.valueOf(100);
+        BigDecimal n2 = BigDecimal.valueOf(10);
+        Assert.assertEquals(BigDecimal.valueOf(10), Calculator.divide(n1, n2));
     }
 
     @Test
     public void multiplyAndDivide () {
-        int n1 = 10;
-        double n2 = Calculator.divide(n1, 3);
-        Assert.assertEquals(10, Calculator.multiply(n2, 3), 0.001);
+        BigDecimal n1 = BigDecimal.valueOf(10);
+        BigDecimal n2 = Calculator.divide(n1, BigDecimal.valueOf(3));
+        Assert.assertEquals(BigDecimal.valueOf(10), Calculator.multiply(n2, BigDecimal.valueOf(3)));
     }
 }

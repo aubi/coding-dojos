bffe960e462b362c70f3240b5565d1aad3cfdffa
NotedSalmon <moreiraandre2001@gmail.com>
2025-05-05 13:38:04 +0100

    Template with string with numbers

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+
+import java.util.Arrays;
+
+/**
+ *  String with numbers seperated by commas
+ *  extract
+ *  work out stats
+ */
+public class Main {
+    public static void main(String[] args) {
+        String numbersToExtract = "12,1,4,52,7,8";
+
+        numbersToExtract.split(",");
+        System.out.println(Arrays.toString(numbersToExtract.split(",")));
+    }
+}
---
+++ src/test/java/NumbersTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class NumbersTest {
+
+    @Test
+    private void Example1(){
+        String numbersToExtract = "12,1,4,52,7,8";
+
+        numbersToExtract.split(",");
+
+    }
+}

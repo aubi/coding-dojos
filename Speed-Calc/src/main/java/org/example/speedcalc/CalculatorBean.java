48f9b516c01117ce197ee31d27f83ecbf696c725
NotedSalmon <moreiraandre2001@gmail.com>
2025-05-12 14:27:22 +0100

    Created template for webpage + bean

---
+++ Speed-Calc/src/main/java/org/example/speedcalc/CalculatorBean.java
@@ -0,0 +1,43 @@
+package org.example.speedcalc;
+
+import jakarta.enterprise.context.SessionScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+
+@Named
+public class CalculatorBean implements Serializable {
+
+    public double getPace() {
+        return pace;
+    }
+
+    public void setPace(double pace) {
+        this.pace = pace;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    private double pace;
+    private double speed;
+
+    @Inject
+    CalculatorService calculator;
+
+    public double calcSpeed(){
+        return calculator.calculatePaceFromKmPerHour(speed);
+    }
+
+    public double calcPace(){
+        return calculator.calculateSpeedKMS(pace);
+    }
+
+
+}

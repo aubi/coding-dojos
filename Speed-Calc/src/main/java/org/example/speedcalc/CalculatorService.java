a68125460487888464f94c8e5a3d9eb9d00584bf
NotedSalmon <moreiraandre2001@gmail.com>
2025-05-12 13:49:31 +0100

    Cacl for pace

--- Speed-Calc/src/main/java/org/example/speedcalc/CalculatorService.java
+++ Speed-Calc/src/main/java/org/example/speedcalc/CalculatorService.java
@@ -3,10 +3,14 @@ package org.example.speedcalc;
 public class CalculatorService {
 
     public double calculateSpeedMS (double pace) {
+
         throw new UnsupportedOperationException();
     }
 
     public double calculatePace (double speedMS) {
+
+        double pace = 1/speedMS;
+        double minPerKm = pace * 1000;
         throw new UnsupportedOperationException();
     }
 }

fcee1dd27a8262a1293e719352b986359a6a001b
Petr Aubrecht <aubrecht@asoftware.cz>
2025-05-13 15:36:49 +0200

    add performance app

---
+++ Performance/src/main/java/fish/payara/performance/Performance.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package fish.payara.performance;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+/**
+ *
+ * @author Petr Aubrecht <aubrecht@asoftware.cz>
+ */
+public class Performance {
+
+    public static void main(String[] args) throws MalformedURLException, IOException {
+        URL url = URI.create("http://localhost:8080/hello-world/").toURL();
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            StringBuilder responseBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                responseBuilder.append(line);
+            }
+            System.out.println(responseBuilder.toString());
+//            assertNotNull(responseBuilder);
+//            assertTrue(responseBuilder.toString()
+//                    .contains("<html>"));
+        }
+    }
+}

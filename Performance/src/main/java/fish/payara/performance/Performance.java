f655f78da83c3b06af238c8fbd81854ac3116ae1
Petr Aubrecht <aubrecht@asoftware.cz>
2025-05-13 16:46:23 +0200

    adding servlet a measure both static file and servlet

--- Performance/src/main/java/fish/payara/performance/Performance.java
+++ Performance/src/main/java/fish/payara/performance/Performance.java
@@ -12,9 +12,6 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URL;
 import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalTime;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -23,13 +20,20 @@ import java.util.concurrent.Executors;
  * @author Petr Aubrecht <aubrecht@asoftware.cz>
  */
 public class Performance {
+    private static final int TRIES = 10_000;
 
     public static void main(String[] args) throws MalformedURLException, IOException {
-        URL url = URI.create("http://localhost:8080/hello-world-0.1-SNAPSHOT/").toURL();
+        measure("");
+        measure("test");
+    }
+
+    private static void measure(String urlPath) throws MalformedURLException, IOException {
+        System.out.println("Measuring path '" + urlPath + "'");
+        URL url = URI.create("http://localhost:8080/hello-world-0.1-SNAPSHOT/" + urlPath).toURL();
 
         long time1 = System.nanoTime();
         try(ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
-            for (int i = 0; i < 1000; ++i) {
+            for (int i = 0; i < TRIES; ++i) {
                 executorService.submit(() -> {
                     try {
                         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -52,8 +56,10 @@ public class Performance {
         }
 
 
-        long timeSpent = (System.nanoTime() - time1) / 1000;
+        long timeSpent = (System.nanoTime() - time1) / TRIES;
         Duration duration = Duration.ofNanos(timeSpent);
         System.out.println("It took " + duration + " seconds");
+
+        System.out.printf("%.3f req/s%n", (1_000_000_000 / (double) timeSpent));
     }
 }

8378865b167246414465fa19b778a947f13373f6
Rohan Parekh <rohan.parekh.in@gmail.com>
2025-06-10 18:27:45 +0530

    initial logic

--- Bowling/src/main/java/fish/payara/bowling/BowlingGame.java
+++ Bowling/src/main/java/fish/payara/bowling/BowlingGame.java
@@ -2,7 +2,15 @@ package fish.payara.bowling;
 
 public class BowlingGame {
 
+    // aaray for turns
+    private int[] rolls = new int[10];
+    private int currentRoll = 0;
+    private int[] frames = new int[3];
+
     public int totalScore() {
+        // collect the score for each frame
+        // add the score for each frame to rolls
+        //totalScore = total of rolls
         return 0;
     }
 }

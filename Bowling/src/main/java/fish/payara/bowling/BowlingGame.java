545c623e4f16b0452f43ae47e8698c3964e342ea
Petr Aubrecht <aubrecht@asoftware.cz>
2025-06-10 15:19:34 +0200

    finish the simplest test case

--- Bowling/src/main/java/fish/payara/bowling/BowlingGame.java
+++ Bowling/src/main/java/fish/payara/bowling/BowlingGame.java
@@ -5,15 +5,19 @@ public class BowlingGame {
     // aaray for turns
     private int[] rolls = new int[21];
     private int currentRoll = 0;
-    private int frames = 10;
-    private int score = 0;
 
-    public int totalScore() {
+    public void roll(int pins) {
+        rolls[currentRoll] = pins;
+        currentRoll++;
+    }
+
+    public int calcTotalScore() {
+        int score = 0;
         // collect the score for each frame
         // add the score for each frame to rolls
         //totalScore = total of rolls
-        for(int i = 0; i < frames; i++) {
-            score += rolls[currentRoll];
+        for (int i = 0; i < currentRoll; i++) {
+            score += rolls[i];
         }
         return score;
     }
--- Bowling/src/test/java/fish/payara/bowling/ScoreTest.java
+++ Bowling/src/test/java/fish/payara/bowling/ScoreTest.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * X X X X X X X X X X X X (12 rolls: 12 strikes) = 10 frames * 30 points = 300
- * 9- 9- 9- 9- 9- 9- 9- 9- 9- 9- (20 rolls: 10 pairs of 9 and miss) = 10 frames * 9 points = 90
- * 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5 (21 rolls: 10 pairs of 5 and spare, with a final 5) = 10 frames * 15 points = 150
+ * 33 33 33 33 33 33 33 33 33 33 (20 rolls: 3 every roll) = 20 frames * 3 points = 60 * X X X X X X X X X X X X (12
+ * rolls: 12 strikes) = 10 frames * 30 points = 300 X X X X X X X X X X X X (12 rolls: 12 strikes) = 10 frames * 30
+ * points = 300 9- 9- 9- 9- 9- 9- 9- 9- 9- 9- (20 rolls: 10 pairs of 9 and miss) = 10 frames * 9 points = 90 * 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5 (21 rolls: 10 pairs of 5 and spare, with a final 5) = 10 frames * 15 points = 150
  *
  */
 public class ScoreTest {
@@ -16,23 +16,20 @@ public class ScoreTest {
     @Test
     public void testStrikeScoreCalculation() {
         BowlingGame game = new BowlingGame();
-
-        for()
-
-        game.totalScore();
-        assertEquals(300, game.totalScore());
+        for (int i = 0; i < 10 + 2; i++) {
+            game.roll(10);
+        }
+        // FIXME: implement strikes and spares
+        assertEquals(300, game.calcTotalScore());
     }
 
     @Test
-    public void testScoreCalculation() {
+    public void testScoreCalculation3Pins() {
         BowlingGame game = new BowlingGame();
         // test for when there is 3 pins for each roll
-        int[] roll = new int[21];
-
-        for()
-
-        for(int i = 0; i < 10; i++) {
-
+        for (int i = 0; i < 2 * 10; i++) {
+            game.roll(3);
         }
+        assertEquals(10 * 2 * 3, game.calcTotalScore());
     }
 }

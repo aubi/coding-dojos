24fd9dc5f546c069416bd27103c6f3604ff0bfaf
NotedSalmon <moreiraandre2001@gmail.com>
2025-06-23 13:39:40 +0100

    Added test for 90 points (9-)

--- Bowling/src/test/java/fish/payara/bowling/ScoreTest.java
+++ Bowling/src/test/java/fish/payara/bowling/ScoreTest.java
@@ -8,7 +8,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * 33 33 33 33 33 33 33 33 33 33 (20 rolls: 3 every roll) = 20 frames * 3 points = 60 * X X X X X X X X X X X X (12
  * rolls: 12 strikes) = 10 frames * 30 points = 300 X X X X X X X X X X X X (12 rolls: 12 strikes) = 10 frames * 30
- * points = 300 9- 9- 9- 9- 9- 9- 9- 9- 9- 9- (20 rolls: 10 pairs of 9 and miss) = 10 frames * 9 points = 90 * 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5 (21 rolls: 10 pairs of 5 and spare, with a final 5) = 10 frames * 15 points = 150
+ * points = 90 9- 9- 9- 9- 9- 9- 9- 9- 9- 9- (20 rolls: 10 pairs of 9 and miss) = 10 frames * 9 points = 90 * 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5 (21 rolls: 10 pairs of 5 and spare, with a final 5) = 10 frames * 15 points = 150
  *
  */
 public class ScoreTest {
@@ -31,5 +31,15 @@ public class ScoreTest {
             game.roll(3);
         }
         assertEquals(10 * 2 * 3, game.calcTotalScore());
+    }
+
+    @Test
+    public void testScore90() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 10; i++) {
+            game.roll(9);
+            game.roll(0);
+        }
+        assertEquals(10 * 9, game.calcTotalScore());
     }
 }

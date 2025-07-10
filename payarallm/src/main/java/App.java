ece674908e0159a82d9250a62e2af0bc162aebee
Patrik <patrik.dudits@payara.fish>
2025-07-10 15:32:46 +0200

    Return response as JSON

--- payarallm/src/main/java/App.java
+++ payarallm/src/main/java/App.java
@@ -8,26 +8,30 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
 
-        String modelName = "smollm:135m";
+        String modelName = "llama3";
 
         OllamaChatModel client = OllamaChatModel.builder()
                 .baseUrl("http://localhost:11434")
                 .modelName(modelName)
                 .build();
 
-        String response = client.generate("What is Payara?");
-
         Scanner scanner = new Scanner(System.in);
         System.out.println("Press Enter to Country...");
         String country = scanner.nextLine();
 
         String response2 =
-                client.generate("List down unique city names which are good for travelling for couples of " + country +
-                        "?");
+                client.generate("List five unique cities names which are good for travelling for couples of " + country +
+                        """
+        . Response should be JSON without any additional text in format:
+        {
+            "cities": [
+                {city: "city1", description: "description1"},
+                ...
+            ]
+        }
+        """);
         System.out.println(response2);
 
-
-        System.out.println(response);
     }
 }
 

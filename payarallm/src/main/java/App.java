136068e601ff3bd08ea15489627f8c7adddb024d
Raushan <raushankumar606@gmail.com>
2025-07-10 18:51:53 +0530

    Enhance App functionality with user input and dynamic model name

--- payarallm/src/main/java/App.java
+++ payarallm/src/main/java/App.java
@@ -1,17 +1,32 @@
 import dev.langchain4j.model.ollama.OllamaChatModel;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
 
+        String modelName = "smollm:135m";
+
         OllamaChatModel client = OllamaChatModel.builder()
                 .baseUrl("http://localhost:11434")
-                .modelName("llama3:latest")
+                .modelName(modelName)
                 .build();
 
         String response = client.generate("What is Payara?");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press Enter to Country...");
+        String country = scanner.nextLine();
+
+        String response2 =
+                client.generate("List down unique city names which are good for travelling for couples of " + country +
+                        "?");
+        System.out.println(response2);
+
+
         System.out.println(response);
     }
 }

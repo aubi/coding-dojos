0a09a94fd41de9940b5af71628b7f6ccbdbdc94c
Marcelo Gonçalves <marcelocg@gmail.com>
2025-07-10 10:02:43 -0300

    Call model for hello world

--- payarallm/src/main/java/App.java
+++ payarallm/src/main/java/App.java
@@ -6,6 +6,15 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        OllamaClient client = new OllamaClient("http://localhost:11434");
+
+        String response = client.chat("ollama", "Hello, how are you?");
+
     }
 }
+
+// Run inside container shell
+// ollama list
+// ollama pull deepseek-r1

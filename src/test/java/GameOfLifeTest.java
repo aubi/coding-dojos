57a77c9232f7549f017c10505efebcba2f9a119b
NotedSalmon <moreiraandre2001@gmail.com>
2025-08-18 13:34:34 +0100

    created test to see if live cell dies if surrounded by less than 2 neighbours

--- src/test/java/GameOfLifeTest.java
+++ src/test/java/GameOfLifeTest.java
@@ -12,6 +12,32 @@ public class GameOfLifeTest {
         Assert.assertEquals(25,grid.returnSize());
     }
 
+    @Test
+    public void testLiveCellDiesWithOneorLessNeighbour() {
+        Grid grid = new Grid(5);
+
+        grid.put(1,1,true);
+        grid.put(2,1,true);
+
+        //Go throught a generation
+        grid.nextGeneration();
+        // check grid 1,1 to make sure it is false
+        Assert.assertEquals(false, grid.get(1,1));
+    }
+
+    @Test
+    public void testDeadCellSpawnsWithinFourNeighbours() {
+        Grid grid = new Grid(5);
+
+        grid.put(1,1,true);
+        grid.put(2,1,true);
+
+        //Go throught a generation
+        grid.nextGeneration();
+        // check grid 1,1 to make sure it is false
+        Assert.assertEquals(false, grid.get(1,1));
+    }
+
     // Test a grid with all alive cells
 //    @Test
 //    public void testGenerate

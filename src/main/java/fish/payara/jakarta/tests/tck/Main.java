3f08320cdfd57e8074b38227376409b2b982fc1c
Rhys Williams <170514543+Viii3@users.noreply.github.com>
2025-09-01 13:58:12 +0100

    Play example simulation on Main.

--- src/main/java/fish/payara/jakarta/tests/tck/Main.java
+++ src/main/java/fish/payara/jakarta/tests/tck/Main.java
@@ -1,7 +1,19 @@
 package fish.payara.jakarta.tests.tck;
 
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        Grid grid = new Grid(25);
+        grid.put(15, 15, true);
+        grid.put(14, 16, true);
+        grid.put(15, 16, true);
+        grid.put(15, 17, true);
+        grid.put(16, 17, true);
+
+        for (int i = 0; i < 50; ++i) {
+            System.out.println(grid);
+            System.out.println();
+            grid = grid.nextGeneration();
+            Thread.sleep(500L);
+        }
     }
 }

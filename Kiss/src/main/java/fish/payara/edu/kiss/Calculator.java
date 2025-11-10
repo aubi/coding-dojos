b33e8d98a97aaec777c2643697af46d4294041c5
Petr Aubrecht <aubrecht@asoftware.cz>
2025-11-10 14:54:21 +0100

    compact format and module import in java 25

--- Kiss/src/main/java/fish/payara/edu/kiss/Calculator.java
+++ Kiss/src/main/java/fish/payara/edu/kiss/Calculator.java
@@ -1,17 +1,8 @@
-package fish.payara.edu.kiss;
 
-import java.util.Arrays;
-import java.util.List;
+import module java.base;
 
-/**
- *
- * @author Petr Aubrecht <aubrecht@asoftware.cz>
- */
-public class Calculator {
-
-    public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        System.out.println("Result is: " + numbers.stream().mapToInt(Integer::intValue).sum());
-    }
+public void main() {
+    List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+    System.out.println("Result is: " + numbers.stream().mapToInt(Integer::intValue).sum());
 }
 

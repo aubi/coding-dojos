9da06a56d165c66602a87ba48f5aae798f9f113b
NotedSalmon <moreiraandre2001@gmail.com>
2025-11-10 13:33:29 +0000

    removed unused class

--- Kiss/src/main/java/fish/payara/edu/kiss/Calculator.java
+++ Kiss/src/main/java/fish/payara/edu/kiss/Calculator.java
@@ -13,8 +13,6 @@ public class Calculator {
 
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
-        
-        NumberProvider<Integer> provider = new ListNumberProvider<>(numbers);
 
         IterativeCalculationStrategy<Integer> strategy = new IterativeCalculationStrategy<>();
         Optional<Integer> result = strategy.calculate(numbers);
@@ -23,27 +21,6 @@ public class Calculator {
                 r -> System.out.println("Result is: " + r),
                 () -> System.err.println("No result produced")
         );
-    }
-}
-
-// Abstract provider interface
-interface NumberProvider<T extends Number> {
-
-    List<T> getNumbers();
-}
-
-class ListNumberProvider<T extends Number> implements NumberProvider<T> {
-
-    private final List<T> data;
-
-    public ListNumberProvider(List<T> data) {
-        // copy for immutability (unnecessary)
-        this.data = new ArrayList<>(data);
-    }
-
-    @Override
-    public List<T> getNumbers() {
-        return new ArrayList<>(data);
     }
 }
 

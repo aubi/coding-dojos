3b1cbf08a8e0756d87d6a24ef7cfd68e6f730d72
Jason Tarry <jason.tarry@payara.fish>
2025-11-11 14:52:32 +0000

    Updated RecordTest and print set

--- java21practice/src/main/java/fish/payara/codingdojo/Main.java
+++ java21practice/src/main/java/fish/payara/codingdojo/Main.java
@@ -1,9 +1,26 @@
 package fish.payara.codingdojo;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
         RecordsTest recordsTest = new RecordsTest("loginpath", "loginname");
         System.out.println(recordsTest);
+        Set<RecordsTest> mutableSet = new HashSet<>();
+        mutableSet.add(recordsTest);
+        mutableSet.add(recordsTest);
+        mutableSet.add(recordsTest);
+        System.out.println(mutableSet);
+        Map<RecordsTest, Integer> map = new HashMap<>();
+        map.put(recordsTest, 1);
+        System.out.println(map);
+        map.put(recordsTest, 2);
+        System.out.println(map);
+        RecordsTest recordsTestPath = new RecordsTest("testpath");
+        System.out.println(recordsTestPath);
     }
 }
--- java21practice/src/main/java/fish/payara/codingdojo/RecordsTest.java
+++ java21practice/src/main/java/fish/payara/codingdojo/RecordsTest.java
@@ -1,5 +1,7 @@
 package fish.payara.codingdojo;
 
 public record RecordsTest(String path, String name) {
-
+    public RecordsTest(String path) {
+        this(path, path + "/" + path);
+    }
 }

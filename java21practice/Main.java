c918a077a1273e4ff91b041157455cc381d2c120
Rohan Parekh <rohan.parekh.in@gmail.com>
2025-11-11 20:02:11 +0530

    Added Main class

---
+++ java21practice/Main.java
@@ -0,0 +1,9 @@
+package java.codingdojo;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RecordsTest recordsTest = new RecordsTest("loginpath", "loginname");
+        System.out.println(recordsTest);
+    }
+}

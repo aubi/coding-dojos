b07f1e0a532861cb9cfeb9cdbcdb4b84af246816
Rohan Parekh <rohan.parekh.in@gmail.com>
2025-11-25 15:33:56 +0530

    New Film

--- java21/src/main/java/Main.java
+++ java21/src/main/java/Main.java
@@ -1,3 +1,24 @@
+import fish.payara.Film;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Main {
 
+    public static void main(String[] args) {
+        ArrayList<String> actorList = new ArrayList<>(Arrays.asList("Leonardo DiCaprio", "Joseph Gordon-Levitt"));
+        Film film1 = new Film("Inception", actorList);
+        System.out.println(film1.hashCode());
+        Set<Film> films = new HashSet<>(Set.of(
+                film1,
+                new Film("Matrix", List.of("Keanu Reaves", "Trinity"))
+        ));
+        actorList.add("Ellen Page");
+        Film film2 = new Film("Inception", actorList);
+        System.out.println(films.contains(film2));
+        System.out.println(film2.hashCode());
+    }
 }
---
+++ java21/src/main/java/fish/payara/Film.java
@@ -0,0 +1,6 @@
+package fish.payara;
+
+import java.util.List;
+
+public record Film(String title, List<String> actor) {
+}
